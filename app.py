f906e0dc576aa21771fbea385669970f07523fca
EC2 Default User <ec2-user@ip-172-31-6-114.ap-south-1.compute.internal>
2026-09-23 19:23:28 +0000

    Add valid Flask app and requirements for assignment completion

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello from Jenkins CI/CD Pipeline!"
+    return "Hello from Vicky's CI/CD Pipeline on AWS ECR!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
